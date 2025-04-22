95f3496094b33a1efb27262d095bca6ed72ae600
gijsbartman <107071577+gijsbartman@users.noreply.github.com>
2025-04-22 09:23:53 +0200

    feat: add reusable LogOutButton component for user logout functionality

---
+++ src/components/auth/logout-button.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Button, buttonVariants } from '@/components/ui/button';
+import { authClient } from '@/lib/auth-client';
+import { cn } from '@/lib/utils';
+import { type VariantProps } from 'class-variance-authority';
+import { useRouter } from 'next/navigation';
+
+export function LogOutButton({
+  className,
+  variant,
+  size,
+  ...props
+}: React.ComponentProps<'button'> & VariantProps<typeof buttonVariants>) {
+  const router = useRouter();
+
+  const onClick = () => {
+    authClient.signOut().then(() => {
+      router.refresh();
+    });
+  };
+
+  return (
+    <Button
+      variant={variant}
+      size={size}
+      onClick={onClick}
+      className={cn('', className)}
+      {...props}
+    >
+      Logout
+    </Button>
+  );
+}
